2cbfcd384fa9e49ebefe2ddff9b8e5741a05004d
Abhinav Mishra <abhinav@abMBP2013.local>
2015-01-10 08:27:01 +0530

    plot1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#read from file and load required data
+file <- "household_power_consumption.txt"
+data <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+inputData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#get what needs to be plotted
+globalActivePower <- as.numeric(inputData$Global_active_power)
+
+#open PNG device
+png("plot1.png", width=480, height=480)
+#plot histogram
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+#close device
+dev.off()
