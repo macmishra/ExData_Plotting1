2c5c9409cf66a806d5ccc897fa295ba9ba984220
Abhinav Mishra <abhinav@abMBP2013.local>
2015-01-10 08:42:35 +0530

    plot2

--- plot2.R
+++ plot2.R
@@ -4,11 +4,12 @@ data <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 inputData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #get what needs to be plotted
+datetime <- strptime(paste(inputData$Date, inputData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 globalActivePower <- as.numeric(inputData$Global_active_power)
 
 #open PNG device
-png("plot1.png", width=480, height=480)
-#plot histogram
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png("plot2.png", width=480, height=480)
+#plot
+plot(datetime, globalActivePower, type = "l", xlab = "" ,ylab="Global Active Power (kilowatts)")
 #close device
 dev.off()
